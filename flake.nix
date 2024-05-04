b3d94fb7872a580f382ada4dddd1fd632ea0bc79
Cole Helbling <cole.e.helbling@outlook.com>
2024-05-04 10:23:14 -0700

    Add flake-compat input

--- flake.nix
+++ flake.nix
@@ -3,8 +3,9 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-compat.url = "github:edolstra/flake-compat";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
--- shell.nix
+++ shell.nix
@@ -1,14 +1,10 @@
-let
-  flake = import
-    (fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/12c64ca55c1014cdc1b16ed5a804aa8576601ff2.tar.gz";
-      sha256 = "0jm6nzb83wa6ai17ly9fzpqc40wg1viib8klq8lby54agpl213w5";
-    })
-    {
-      src = builtins.filterSource
-        (path: _: baseNameOf path != ".git"
-          || baseNameOf path != "target"
-          || baseNameOf path != "result") ./.;
-    };
-in
-flake.shellNix.default
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).defaultNix
