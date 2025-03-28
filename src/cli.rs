4328b5a4b65e0a0e27da020f9fd70177110427fb
Cole Helbling <cole.e.helbling@outlook.com>
2025-03-28 14:59:12 -0700

    cli: better error message when current_dir() fails

--- src/cli.rs
+++ src/cli.rs
@@ -352,7 +352,7 @@ where
     let path = path.as_ref();
     let relative_path = current_path
         .strip_prefix(&conf.root)
-        .unwrap_or(&env::current_dir()?)
+        .unwrap_or(&env::current_dir().wrap_err("Failed to get current directory")?)
         .join(path);
     let recipients = self::get_recipients_from_config(&conf, &relative_path)
         .wrap_err("Failed to get recipients from config file")?;
